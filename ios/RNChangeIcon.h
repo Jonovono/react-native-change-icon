7114cb0be3eef088dd1be36ed3d7021d06777a8e
Jordan Howlett <jordan@howlett.io>
2020-02-15 17:27:51 -0500

    TEST

--- ios/RNChangeIcon.h
+++ ios/RNChangeIcon.h
@@ -1,12 +1,5 @@
-
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
 #import <React/RCTBridgeModule.h>
-#endif
-#import <UIKit/UIKit.h>
 
-@interface RNChangeIcon : NSObject <RCTBridgeModule, UIApplicationDelegate>
+@interface RNChangeIcon : NSObject <RCTBridgeModule>
 
 @end
-  
--- ios/RNChangeIcon.m
+++ ios/RNChangeIcon.m
@@ -1,17 +1,13 @@
 #import "React/RCTLog.h"
-
 #import "RNChangeIcon.h"
 
 @implementation RNChangeIcon
 
-- (dispatch_queue_t)methodQueue
-{
-    return dispatch_get_main_queue();
-}
 RCT_EXPORT_MODULE()
 
-RCT_REMAP_METHOD(changeIcon, iconName:(NSString *)iconName resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
-    NSError *error = nil;
+RCT_EXPORT_METHOD(changeIcon:iconName:(NSString *)iconName resolver:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+{
+      NSError *error = nil;
     // Not supported
     if ([[UIApplication sharedApplication] supportsAlternateIcons] == NO) {
         reject(@"Error", @"Alternate icon not supported", error);
