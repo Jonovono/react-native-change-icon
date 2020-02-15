3230b9c30d96fe9e7b926bd1ea1ab8f185a076da
Jordan Howlett <jordan@howlett.io>
2020-02-15 17:44:37 -0500

    TST

--- ios/RNChangeIcon.m
+++ ios/RNChangeIcon.m
@@ -5,12 +5,12 @@
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(changeIcon:iconName:(NSString *)iconName resolver:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+RCT_EXPORT_METHOD(changeIcon:iconName:(NSString *)iconName)
 {
       NSError *error = nil;
     // Not supported
     if ([[UIApplication sharedApplication] supportsAlternateIcons] == NO) {
-        reject(@"Error", @"Alternate icon not supported", error);
+        // reject(@"Error", @"Alternate icon not supported", error);
         RCTLog(@"Alternate icons are not supported");
         return;
     }
@@ -18,12 +18,12 @@ RCT_EXPORT_METHOD(changeIcon:iconName:(NSString *)iconName resolver:(RCTPromiseR
     NSString *currentIcon = [[UIApplication sharedApplication] alternateIconName];
 
     if ([iconName isEqualToString:currentIcon]) {
-        reject(@"Error", @"Icon already in use", error);
+        // reject(@"Error", @"Icon already in use", error);
         RCTLog(@"Icon already in use");
         return;
     }
 
-    resolve(@(YES));
+    // resolve(@(YES));
 
     // Custom icon
     [[UIApplication sharedApplication] setAlternateIconName:iconName completionHandler:^(NSError * _Nullable error) {
